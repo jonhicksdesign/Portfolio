b5b55ad2ec9235042071a4f1b00c7b2d34130f6a
jonhicksdesign <70168774+jonhicksdesign@users.noreply.github.com>
2020-09-25 15:06:25 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  $('.slider-area').slick({
+    dots: false,
+    accessibility: true,
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    centerMode: false,
+    slidesToShow: 10,
+    slidesToScroll: 8,
+    variableWidth: true,
+    touchMove: true,
+    useCSS: true,
+    cssEase: 'ease',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          prevArrow: false,
+          nextArrow: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+$(document).on('ready', function() {
+   $('.slider-area').slickLightbox({
+    src: 'src',
+    itemSelector: '.pic img',
+});
+});
